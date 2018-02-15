4fe2522be93497769e379ceb38f74584e935ed77
Archana Sankaranarayanan <asankar@netflix.com>
2018-02-15 12:46:11 -0800

    First set of files moved from spinnaker/core.

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp')
+
+/* Build styleguide */
+gulp.task('build:styleguide', function () {
+    const concat = require('gulp-concat');
+    const postcss = require('gulp-postcss');
+    const autoprefixer = require('autoprefixer');
+    const customProperties = require('postcss-css-variables');
+    const Import = require('postcss-import');
+    const styleGuide = require('postcss-style-guide');
+    const extend = require('postcss-extend');
+    const nano = require('cssnano');
+    const exportVars = require('postcss-export-vars');
+
+    return gulp.src('./src/styles/app.css')
+      .pipe(postcss([
+          Import,
+          extend,
+          exportVars({
+            type: 'json',
+            file: './public/css-vars.json'
+          }),
+          customProperties({ preserve: true }),
+          autoprefixer,
+          styleGuide({
+              project: 'Spinnaker',
+              dest: './public/styleguide.html',
+              showCode: false,
+              themePath: './src/styleguide-template/'
+          }),
+          nano
+      ]))
+      .pipe(concat('./public/styleguide.min.css'))
+      .pipe(gulp.dest('..'))
+})
+
+gulp.task('default', function() {
+  gulp.watch(['./src/styles/*.css', './src/styleguide-template/*.*'], 
+    ['build:styleguide']);
+})
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Spinnaker Styleguide
+This is the collection of reusable Spinnaker patterns.
+
+## What does it include?
+- A collection of CSS files with reusable UI elements
+- Style guide generator (gulpfile.js)
+- Style guide generated in public/ directory
+- Style guide template in src/styleguide-template
