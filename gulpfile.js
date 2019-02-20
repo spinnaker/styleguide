f567a535002fd7251d094ef33c7cb14cb15ddb78
Archana Sankaranarayanan <asankar@netflix.com>
2019-02-19 16:42:36 -0800

    feat(pillsAndTags): Added counters

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,45 @@
 const gulp = require('gulp')
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const customProperties = require('postcss-css-variables');
+const Import = require('postcss-import');
+const styleGuide = require('postcss-style-guide');
+const extend = require('postcss-extend');
+const nano = require('cssnano');
+const extractVars = require('postcss-extract-vars');
 
-/* Build styleguide */
-gulp.task('build:styleguide', function () {
-    const concat = require('gulp-concat');
-    const postcss = require('gulp-postcss');
-    const autoprefixer = require('autoprefixer');
-    const customProperties = require('postcss-css-variables');
-    const Import = require('postcss-import');
-    const styleGuide = require('postcss-style-guide');
-    const extend = require('postcss-extend');
-    const nano = require('cssnano');
-    const extractVars = require('postcss-extract-vars');
+function styles() {
+  return gulp.src('./src/styles/app.css')
+    .pipe(postcss([
+      Import,
+      extend,
+      extractVars({
+        file: './public/customVars.css'
+      }),
+      customProperties({ preserve: true }),
+      autoprefixer,
+      styleGuide({
+        project: 'Spinnaker',
+        dest: './public/styleguide.html',
+        showCode: false,
+        themePath: './src/styleguide-template/'
+      }),
+      nano
+    ]))
+    .pipe(concat('./public/styleguide.min.css'))
+    .pipe(gulp.dest('.'));
+}
 
-    return gulp.src('./src/styles/app.css')
-      .pipe(postcss([
-          Import,
-          extend,
-          extractVars({
-            file: './public/customVars.css'
-          }),
-          customProperties({ preserve: true }),
-          autoprefixer,
-          styleGuide({
-              project: 'Spinnaker',
-              dest: './public/styleguide.html',
-              showCode: false,
-              themePath: './src/styleguide-template/'
-          }),
-          nano
-      ]))
-      .pipe(concat('./public/styleguide.min.css'))
-      .pipe(gulp.dest('.'))
-})
+let build = gulp.series(styles);
 
-gulp.task('default', function() {
-  gulp.watch(['./src/styles/*.css', './src/styleguide-template/*.*'], 
-    ['build:styleguide']);
-})
+function watch() {
+  gulp.watch(['./src/styles/*.css', './src/styleguide-template/*.*'],
+    build);
+}
+ 
+exports.watch = watch;
+exports.styles = styles;
+
+gulp.task('default', watch);
+gulp.task('build', build);
